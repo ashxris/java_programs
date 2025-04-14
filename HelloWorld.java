4a87ba5dd70fef0e4d5fcfc7df1da0d716b44672
Ashwin Krishna <krishnaashwin2004@gmail.com>
2025-04-14 14:49:14 +0530

    First Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    //THIS IS MY FIRST JAVA PROGRAM
+    /* This is also a comment */
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
