93fc7ee5e7c4f118ed1506e36b269cfff55c01be
Ashwin Krishna <krishnaashwin2004@gmail.com>
2025-04-14 23:23:51 +0530

    Datatypes in Java

---
+++ datatypes_in_java.java
@@ -0,0 +1,16 @@
+public class datatypes_in_java {
+    public static void main (String[] args){
+
+        int age = 45;
+        double principle = 47999.50;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println(age);
+        System.out.println("The principle is " + principle);
+        System.out.println(grade);
+        System.out.println(isStudent);
+
+
+    }
+}
