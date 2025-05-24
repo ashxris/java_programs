31d56f0c815b2e0895342af05d5065a6ff57d232
Ashwin Krishna <krishnaashwin2004@gmail.com>
2025-05-24 23:21:04 +0530

    Add for loop implementation to print numbers in ascending and descending order

---
+++ for_loop.java
@@ -0,0 +1,16 @@
+public class for_loop {
+    public static void main(String[] args){
+
+        for(int i = 0; i<10;i++){
+            System.err.println(i);
+        }
+
+        System.out.println("\n");
+
+        for(int j=10; j>0;j--){
+            System.err.println(j);
+        }
+        
+    }
+    
+}
