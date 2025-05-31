f520326249175489d584af65ae4187a7a3f12d13
Ashwin Krishna <krishnaashwin2004@gmail.com>
2025-05-31 20:05:12 +0530

    Add objects and oops classes to demonstrate object-oriented programming concepts

---
+++ objects.java
@@ -0,0 +1,10 @@
+public class objects {
+    public static void main (String[] args){
+
+        oops car = new oops();
+        System.out.println(car.model);
+        System.out.println(car.car);
+
+        car.start();
+    }
+}
---
+++ oops.java
@@ -0,0 +1,12 @@
+public class oops {
+
+    String car = "Toyota";
+    String model = "Hilux";
+    int year = 2025;
+    boolean isRunning = true;
+
+    void start(){
+        System.out.println("Your "+model+" is running.");
+    }
+    
+}
